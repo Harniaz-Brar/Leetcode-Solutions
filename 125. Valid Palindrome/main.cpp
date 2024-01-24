326b3573fd63c236dc6715c1ad5b50be7b2768b7
Harniaz Brar <harniazbrar@gmail.com>
2024-01-24 16:28:32 -0500

    Add files via upload

---
+++ 125. Valid Palindrome/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class Solution {
+private:
+    bool valid(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    char toLowerCase(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+            return ch;
+        } else {
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+
+    bool checkPalindrome(std::string a) {
+        int n = a.length();
+        int s = 0;
+        int e = n - 1;
+
+        while (s <= e) {
+            if (a[s] != a[e]) {
+                return false;
+            } else {
+                s++;
+                e--;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isPalindrome(std::string s) {
+        std::string temp = "";
+
+        for (int j = 0; j < s.length(); j++) {
+            if (valid(s[j])) {
+                temp.push_back(s[j]);
+            }
+        }
+
+        for (int j = 0; j < temp.length(); j++) {
+            temp[j] = toLowerCase(temp[j]);
+        }
+
+        return checkPalindrome(temp);
+    }
+};
